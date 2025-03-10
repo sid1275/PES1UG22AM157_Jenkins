7d312e94e4bdfc8f4a04ec7937c95924785698bd
Siddharth <sidd10.gandhi@gmail.com>
2025-03-10 16:26:46 +0530

    sid

--- main/hello.cpp
+++ main/hello.cpp
@@ -6,5 +6,6 @@ int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
   cout << "I have successfully built and run this  \n";
+  cout << "My name is Siddharth Gandhi  \n";
   return 0;
 }
